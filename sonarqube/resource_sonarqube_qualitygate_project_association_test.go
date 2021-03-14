ddb291ea5c82679d2165e3591d3eb0f95b63fe26
Joel Damata <joel.damata94@gmail.com>
2021-03-14 17:15:45 -0400

    fixes qualitygate project association for 7.9

--- sonarqube/resource_sonarqube_qualitygate_project_association_test.go
+++ sonarqube/resource_sonarqube_qualitygate_project_association_test.go
@@ -2,6 +2,7 @@ package sonarqube
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -19,7 +20,56 @@ func testSweepSonarqubeQualitygateProjectAssociationSweeper(r string) error {
 	return nil
 }
 
-func testAccSonarqubeQualitygateProjectAssociationBasicConfig(rnd string, name string) string {
+func testAccSonarqubeQualitygateProjectAssociationBasicGateName(rnd string, name string) string {
+	return fmt.Sprintf(`
+		resource "sonarqube_qualitygate" "%[1]s" {
+			name = "%[2]s"
+		}
+
+		resource "sonarqube_project" "%[1]s" {
+			name       = "%[2]s"
+			project    = "%[2]s"
+			visibility = "public"
+		}
+
+		resource "sonarqube_qualitygate_project_association" "%[1]s" {
+			gatename   = sonarqube_qualitygate.%[1]s.name
+			projectkey = sonarqube_project.%[1]s.project
+		}`, rnd, name)
+}
+
+func TestAccSonarqubeQualitygateProjectAssociationGateName(t *testing.T) {
+	rnd := generateRandomResourceName()
+	name := "sonarqube_qualitygate_project_association." + rnd
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSonarqubeQualitygateProjectAssociationBasicGateName(rnd, "testAccSonarqubeProjectAssociation"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "gatename", "testAccSonarqubeProjectAssociation"),
+					resource.TestCheckResourceAttr(name, "projectkey", "testAccSonarqubeProjectAssociation"),
+				),
+			},
+			{
+				ResourceName:      name,
+				ImportState:       true,
+				ImportStateVerify: true,
+				// If sonarqube version is <8.0, this test case will fail because of a missing gateid
+				ExpectError: regexp.MustCompile("Error: API returned an error: The 'gateId' parameter is missing"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "gatename", "testAccSonarqubeProjectAssociation"),
+					resource.TestCheckResourceAttr(name, "projectkey", "testAccSonarqubeProjectAssociation"),
+				),
+			},
+		},
+	})
+}
+
+// Test for sonarqube version 7.9
+func testAccSonarqubeQualitygateProjectAssociationBasicGateID(rnd string, name string) string {
 	return fmt.Sprintf(`
 		resource "sonarqube_qualitygate" "%[1]s" {
 			name = "%[2]s"
@@ -32,12 +82,12 @@ func testAccSonarqubeQualitygateProjectAssociationBasicConfig(rnd string, name s
 		}
 
 		resource "sonarqube_qualitygate_project_association" "%[1]s" {
-			gatename   = sonarqube_qualitygate.%[1]s.name
+			gateid     = sonarqube_qualitygate.%[1]s.id
 			projectkey = sonarqube_project.%[1]s.project
 		}`, rnd, name)
 }
 
-func TestAccSonarqubeQualitygateProjectAssociationBasic(t *testing.T) {
+func TestAccSonarqubeQualitygateProjectAssociationGateID(t *testing.T) {
 	rnd := generateRandomResourceName()
 	name := "sonarqube_qualitygate_project_association." + rnd
 
@@ -46,9 +96,9 @@ func TestAccSonarqubeQualitygateProjectAssociationBasic(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccSonarqubeQualitygateProjectAssociationBasicConfig(rnd, "testAccSonarqubeProjectAssociation"),
+				Config: testAccSonarqubeQualitygateProjectAssociationBasicGateID(rnd, "testAccSonarqubeProjectAssociation"),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(name, "gatename", "testAccSonarqubeProjectAssociation"),
+					resource.TestCheckResourceAttrSet(name, "gateid"),
 					resource.TestCheckResourceAttr(name, "projectkey", "testAccSonarqubeProjectAssociation"),
 				),
 			},
@@ -57,7 +107,7 @@ func TestAccSonarqubeQualitygateProjectAssociationBasic(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(name, "gatename", "testAccSonarqubeProjectAssociation"),
+					resource.TestCheckResourceAttrSet(name, "gateid"),
 					resource.TestCheckResourceAttr(name, "projectkey", "testAccSonarqubeProjectAssociation"),
 				),
 			},
