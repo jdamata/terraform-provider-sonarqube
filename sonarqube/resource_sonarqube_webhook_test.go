b4a4ffdaaf4812dfd2155d48fe39a4b2aa15c3cc
Michael Parker <michael@parker.gg>
2021-06-01 19:18:12 -0500

    update test

--- sonarqube/resource_sonarqube_webhook_test.go
+++ sonarqube/resource_sonarqube_webhook_test.go
@@ -40,6 +40,42 @@ func TestAccSonarqubeWebhookBasic(t *testing.T) {
 	})
 }
 
+func TestAccSonarqubeWebhookUpdate(t *testing.T) {
+	rnd := generateRandomResourceName()
+	resourceName := "sonarqube_webhook." + rnd
+
+	firstName := acctest.RandString(10)
+	firstUrl := fmt.Sprintf("https://%s.com", acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	firstSecret := acctest.RandString(10)
+
+	secondName := acctest.RandString(10)
+	secondUrl := fmt.Sprintf("https://%s.com", acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	secondSecret := acctest.RandString(10)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSonarqubeWebhookBasicConfig(rnd, firstName, firstUrl, firstSecret),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", firstName),
+					resource.TestCheckResourceAttr(resourceName, "url", firstUrl),
+					resource.TestCheckResourceAttr(resourceName, "secret", firstSecret),
+				),
+			},
+			{
+				Config: testAccSonarqubeWebhookBasicConfig(rnd, secondName, secondUrl, secondSecret),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", secondName),
+					resource.TestCheckResourceAttr(resourceName, "url", secondUrl),
+					resource.TestCheckResourceAttr(resourceName, "secret", secondSecret),
+				),
+			},
+		},
+	})
+}
+
 func testAccSonarqubeWebhookBasicConfig(rnd, name, url, secret string) string {
 	return fmt.Sprintf(`
 resource "sonarqube_webhook" "%s" {
