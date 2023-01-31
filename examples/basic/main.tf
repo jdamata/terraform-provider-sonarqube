652fcfaf6d5dae16225f17bd8271d0f8a33b9586
ViktorLindgren95 <viktor.lindgren@snowsoftware.com>
2023-01-31 16:01:15 +0100

    Reverted back

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,23 +1,19 @@
 terraform {
   required_providers {
     sonarqube = {
-      source = "terraform-example.com/exampleprovider/example"
+      source = "jdamata/sonarqube"
     }
   }
 }
 
 provider "sonarqube" {
-  user = "admin"
-  pass = "admin1"
-  host = "http://localhost:9001/"
+    user   = "admin"
+    pass   = "admin" 
+    host   = "http://127.0.0.1:9000"
 }
 
 resource "sonarqube_project" "tf-postfix-test" {
-  name       = "tf-postfix"
-  project    = "tf-postfix"
+  name       = "tf-postfix-test"
+  project    = "tf-postfix-test"
   visibility = "public"
 }
-#resource "sonarqube_project_main_branch" "mybranch" {
-#  name    = "main3"
-#  project = sonarqube_project.tf-postfix-test.project
-#}
