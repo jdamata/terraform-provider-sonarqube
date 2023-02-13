1526b4727f75704703fda02338221b8a60e1ee3e
Jacob Williams <jacob.williams@freenome.com>
2023-02-13 16:20:13 -0500

    Implement the GitLab binding and ALM APIs

---
+++ sonarqube/resource_sonarqube_alm_gitlab.go
@@ -0,0 +1,162 @@
+package sonarqube
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+// GetAlmGitlab for unmarshalling response body from alm list definitions. With only gitlab populated
+type GetAlmGitlab struct {
+	Gitlab []struct {
+		Key                 string `json:"key"`
+		URL                 string `json:"url"`
+		PersonalAccessToken string `json:"personalAccessToken,omitempty"`
+	} `json:"gitlab"`
+}
+
+// Returns the resource represented by this file.
+func resourceSonarqubeAlmGitlab() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSonarqubeAlmGitlabCreate,
+		Read:   resourceSonarqubeAlmGitlabRead,
+		Update: resourceSonarqubeAlmGitlabUpdate,
+		Delete: resourceSonarqubeAlmGitlabDelete,
+
+		// Define the fields of this schema.
+		Schema: map[string]*schema.Schema{
+			"key": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringLenBetween(1, 200)),
+			},
+			"personal_access_token": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringLenBetween(1, 2000)),
+			},
+			"url": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringLenBetween(1, 2000)),
+			},
+		},
+	}
+}
+
+func resourceSonarqubeAlmGitlabCreate(d *schema.ResourceData, m interface{}) error {
+	sonarQubeURL := m.(*ProviderConfiguration).sonarQubeURL
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/alm_settings/create_gitlab"
+
+	sonarQubeURL.RawQuery = url.Values{
+		"key":                 []string{d.Get("key").(string)},
+		"personalAccessToken": []string{d.Get("personal_access_token").(string)},
+		"url":                 []string{d.Get("url").(string)},
+	}.Encode()
+
+	resp, err := httpRequestHelper(
+		m.(*ProviderConfiguration).httpClient,
+		"POST",
+		sonarQubeURL.String(),
+		http.StatusNoContent,
+		"resourceSonarqubeAlmGitlabCreate",
+	)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	d.SetId(d.Get("key").(string))
+
+	return resourceSonarqubeAlmGitlabRead(d, m)
+}
+
+func resourceSonarqubeAlmGitlabRead(d *schema.ResourceData, m interface{}) error {
+	sonarQubeURL := m.(*ProviderConfiguration).sonarQubeURL
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/alm_settings/list_definitions"
+	sonarQubeURL.RawQuery = url.Values{}.Encode() // Dunno if you can keep it empty tbh?
+
+	resp, err := httpRequestHelper(
+		m.(*ProviderConfiguration).httpClient,
+		"GET",
+		sonarQubeURL.String(),
+		http.StatusOK,
+		"resourceSonarqubeAlmGitlabRead",
+	)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Decode response into struct
+	AlmGitlabReadResponse := GetAlmGitlab{}
+	err = json.NewDecoder(resp.Body).Decode(&AlmGitlabReadResponse)
+	if err != nil {
+		return fmt.Errorf("resourceSonarqubeAlmGitlabRead: Failed to decode json into struct: %+v", err)
+	}
+	// Loop over all GitHub instances to see if the Alm instance exists.
+	for _, value := range AlmGitlabReadResponse.Gitlab {
+		if d.Id() == value.Key {
+			d.Set("key", value.Key)
+			d.Set("url", value.URL)
+			d.Set("personal_access_token", value.PersonalAccessToken)
+			return nil
+		}
+	}
+	return fmt.Errorf("resourceSonarqubeGitlabBindingRead: Failed to find gitlab binding: %+v", d.Id())
+
+}
+func resourceSonarqubeAlmGitlabUpdate(d *schema.ResourceData, m interface{}) error {
+	sonarQubeURL := m.(*ProviderConfiguration).sonarQubeURL
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/alm_settings/update_gitlab"
+	sonarQubeURL.RawQuery = url.Values{
+		"key":                 []string{d.Id()},
+		"newKey":              []string{d.Get("key").(string)},
+		"personalAccessToken": []string{d.Get("personal_access_token").(string)},
+		"url":                 []string{d.Get("url").(string)},
+	}.Encode()
+
+	resp, err := httpRequestHelper(
+		m.(*ProviderConfiguration).httpClient,
+		"POST",
+		sonarQubeURL.String(),
+		http.StatusOK,
+		"resourceSonarqubeAlmGitlabUpdate",
+	)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return resourceSonarqubeAlmGitlabRead(d, m)
+}
+
+func resourceSonarqubeAlmGitlabDelete(d *schema.ResourceData, m interface{}) error {
+	sonarQubeURL := m.(*ProviderConfiguration).sonarQubeURL
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/alm_settings/delete"
+	sonarQubeURL.RawQuery = url.Values{
+		"key": []string{d.Get("key").(string)},
+	}.Encode()
+
+	resp, err := httpRequestHelper(
+		m.(*ProviderConfiguration).httpClient,
+		"POST",
+		sonarQubeURL.String(),
+		http.StatusNoContent,
+		"resourceSonarqubeAlmGitlabDelete",
+	)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
---
+++ sonarqube/resource_sonarqube_gitlab_binding.go
@@ -0,0 +1,144 @@
+package sonarqube
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// Returns the resource represented by this file.
+func resourceSonarqubeGitlabBinding() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSonarqubeGitlabBindingCreate,
+		Read:   resourceSonarqubeGitlabBindingRead,
+		Delete: resourceSonarqubeGitlabBindingDelete,
+		Importer: &schema.ResourceImporter{
+			State: resourceSonarqubeGitlabBindingImport,
+		},
+		// Define the fields of this schema.
+		Schema: map[string]*schema.Schema{
+			"alm_setting": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"monorepo": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+				ForceNew: true,
+			},
+			"project": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"repository": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceSonarqubeGitlabBindingCreate(d *schema.ResourceData, m interface{}) error {
+	sonarQubeURL := m.(*ProviderConfiguration).sonarQubeURL
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/alm_settings/set_gitlab_binding"
+
+	sonarQubeURL.RawQuery = url.Values{
+		"almSetting":            []string{d.Get("alm_setting").(string)},
+		"monorepo":              []string{d.Get("monorepo").(string)},
+		"project":               []string{d.Get("project").(string)},
+		"repository":            []string{d.Get("repository").(string)},
+	}.Encode()
+
+	resp, err := httpRequestHelper(
+		m.(*ProviderConfiguration).httpClient,
+		"POST",
+		sonarQubeURL.String(),
+		http.StatusNoContent,
+		"resourceSonarqubeGitlabBindingCreate",
+	)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	id := fmt.Sprintf("%v/%v", d.Get("project").(string), d.Get("repository").(string))
+	d.SetId(id)
+
+	return resourceSonarqubeGitlabBindingRead(d, m)
+}
+
+func resourceSonarqubeGitlabBindingRead(d *schema.ResourceData, m interface{}) error {
+	idSlice := strings.SplitN(d.Id(), "/", 2)
+	sonarQubeURL := m.(*ProviderConfiguration).sonarQubeURL
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/alm_settings/get_binding"
+	sonarQubeURL.RawQuery = url.Values{
+		"project": []string{idSlice[0]},
+	}.Encode()
+
+	resp, err := httpRequestHelper(
+		m.(*ProviderConfiguration).httpClient,
+		"GET",
+		sonarQubeURL.String(),
+		http.StatusOK,
+		"resourceSonarqubeGitlabBindingRead",
+	)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Decode response into struct
+	BindingReadResponse := GetBinding{}
+	err = json.NewDecoder(resp.Body).Decode(&BindingReadResponse)
+	if err != nil {
+		return fmt.Errorf("resourceSonarqubeGitlabBindingRead: Failed to decode json into struct: %+v", err)
+	}
+	// Loop over all branches to see if the main branch we need exists.
+	if idSlice[1] == BindingReadResponse.Repository && BindingReadResponse.Alm == "gitlab" {
+		d.Set("project", idSlice[0])
+		d.Set("repository", idSlice[1])
+		d.Set("alm_setting", BindingReadResponse.Key)
+		d.Set("monorepo", strconv.FormatBool(BindingReadResponse.Monorepo))
+
+		return nil
+	}
+	return fmt.Errorf("resourceSonarqubeGitlabBindingRead: Failed to find gitlab binding: %+v", d.Id())
+}
+
+func resourceSonarqubeGitlabBindingDelete(d *schema.ResourceData, m interface{}) error {
+	sonarQubeURL := m.(*ProviderConfiguration).sonarQubeURL
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/alm_settings/delete_binding"
+	sonarQubeURL.RawQuery = url.Values{
+		"project": []string{d.Get("project").(string)},
+	}.Encode()
+
+	resp, err := httpRequestHelper(
+		m.(*ProviderConfiguration).httpClient,
+		"POST",
+		sonarQubeURL.String(),
+		http.StatusNoContent,
+		"resourceSonarqubeGitlabBindingDelete",
+	)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func resourceSonarqubeGitlabBindingImport(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
+	if err := resourceSonarqubeGitlabBindingRead(d, m); err != nil {
+		return nil, err
+	}
+	return []*schema.ResourceData{d}, nil
+}
