28e622b56a3babbd4ec5c20e62a2c9f40080033f
Jacob Williams <jacob.williams@freenome.com>
2023-03-13 17:36:47 -0400

    User tokens can be created for any login_name (with admin privileges)

--- sonarqube/resource_sonarqube_user_token.go
+++ sonarqube/resource_sonarqube_user_token.go
@@ -101,12 +101,12 @@ func resourceSonarqubeUserTokenCreate(d *schema.ResourceData, m interface{}) err
 		"type": []string{string(tokenType)},
 	}
 
-	if tokenType == UserToken {
-		loginName := d.Get("login_name").(string)
-		if loginName != "" {
-			rawQuery.Add("login", loginName)
-		}
-	} else if tokenType == ProjectAnalysisToken {
+	loginName := d.Get("login_name").(string)
+	if loginName != "" {
+		rawQuery.Add("login", loginName)
+	}
+
+	if tokenType == ProjectAnalysisToken {
 		projectKey := d.Get("project_key").(string)
 		if projectKey == "" {
 			return fmt.Errorf("resourceSonarqubeUserTokenCreate: 'project_key' must be configured when the token 'type' is %s", ProjectAnalysisToken)
