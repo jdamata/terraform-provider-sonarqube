8f34ccb82dc9d754f3aa6f1932d41c08381d76cb
Joel Damata <joel.damata94@gmail.com>
2023-07-28 05:25:07 +0000

    wip fix webhook test on latest tag

--- sonarqube/resource_sonarqube_webhook.go
+++ sonarqube/resource_sonarqube_webhook.go
@@ -120,7 +120,11 @@ func resourceSonarqubeWebhookRead(d *schema.ResourceData, m interface{}) error {
 		if webhook.Key == d.Id() {
 			d.Set("name", webhook.Name)
 			d.Set("url", webhook.Url)
-			d.Set("secret", webhook.Secret)
+			// Version 10.1 of sonarqube does not return the secret in the api response anymore. Field 'secret' replaced by flag 'hasSecret' in response
+			// Instead we just set the secret in state to the value being passed in to avoid constant drifts
+			if secret, ok := d.GetOk("secret"); ok {
+				d.Set("secret", secret.(string))
+			}
 			return nil
 		}
 	}
