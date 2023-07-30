764c9567e6480303a6b86d7942c70fe6dec4262e
Joel Damata <joel.damata94@gmail.com>
2023-07-30 04:30:10 +0000

    test

--- sonarqube/resource_sonarqube_qualityprofile_project_association.go
+++ sonarqube/resource_sonarqube_qualityprofile_project_association.go
@@ -130,11 +130,16 @@ func resourceSonarqubeQualityProfileProjectAssociationRead(d *schema.ResourceDat
 
 	var qualityProfileID string
 	for _, value := range getQualityProfileResponse.Profiles {
+		fmt.Printf("JOEL TESTING. THIS IS THE NAME OF THE QUALITY PROFILE FROM THE ID: %+v", idSlice[0])
+		fmt.Printf("JOEL TESTING. THIS IS THE NAME OF THE QUALITY PROFILE FROM THE API RESULTS: %+v", value.Name)
 		if idSlice[0] == value.Name {
 			qualityProfileID = value.Key
 			language = value.Language
 			qualityProfile = value.Name
 		}
+	}
+	if qualityProfileID == "" {
+		return fmt.Errorf("resourceSonarqubeQualityProfileProjectAssociationRead: Failed to find a quality profile ID from quality profile name: %+v", d.Id())
 	}
 
 	// With the qualityProfileID we can check if the project name is associated
