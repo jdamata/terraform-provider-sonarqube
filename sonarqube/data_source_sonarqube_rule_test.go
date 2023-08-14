6ac661bc9c8a9f0a6e91e3d4c7ef262a418d9b59
felixlut <felix.luthman@gmail.com>
2023-08-14 20:19:51 +0200

    upgrade to terraform-plugin-testing go module

--- sonarqube/data_source_sonarqube_rule_test.go
+++ sonarqube/data_source_sonarqube_rule_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func testAccSonarqubeRuleDataSourceConfig(rnd string, custom_key string, markdown_description string, name string, template_key string, severity string, status string, type_p string) string {
