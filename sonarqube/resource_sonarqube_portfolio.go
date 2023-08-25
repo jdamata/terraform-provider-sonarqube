3cb74a27352a27d48ae6960b21d94c33285e4087
pnag90 <pnag90@gmail.com>
2023-08-25 16:39:12 +0100

    Fix identation (go fmt)

--- sonarqube/resource_sonarqube_portfolio.go
+++ sonarqube/resource_sonarqube_portfolio.go
@@ -71,9 +71,9 @@ func resourceSonarqubePortfolio() *schema.Resource {
 				ValidateFunc: validation.StringInSlice([]string{"NONE", "MANUAL", "TAGS", "REGEXP", "REST"}, false),
 			},
 			"branch": { // Only active for TAGS, REGEXP and REST
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: false,
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    false,
 				Description: "Which branch to analyze. If nothing, or '' is specified, the main branch is used.",
 			},
 			"tags": { // Only active for TAGS
