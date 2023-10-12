383b3040a95a7a4843d7af22b1f8c75ca0fb4670
felixlut <felix.luthman@snowsoftware.com>
2023-10-12 11:22:28 +0200

    run nested test

--- sonarqube/resource_sonarqube_portfolio_test.go
+++ sonarqube/resource_sonarqube_portfolio_test.go
@@ -417,6 +417,10 @@ func TestAccSonarqubePortfolioManualProjects(t *testing.T) {
 					resource.TestCheckResourceAttr(name, "key", "testAccSonarqubePortfolioKey"),
 					resource.TestCheckResourceAttr(name, "selection_mode", "MANUAL"),
 					resource.TestCheckResourceAttr(name, "selected_projects.#", "1"),
+					resource.TestCheckTypeSetElemNestedAttrs(name, "selected_projects.*", map[string]string{
+						"project_key": "testAccSonarqubeProjectKey",
+						"selected_branches.#": "1",
+					}),
 					// resource.TestCheckResourceAttrSet(name, "selected_projects"),
 					// resource.TestCheckResourceAttr(name, "selected_projects.0.project_key", "testAccSonarqubeProjectKey"),
 					// resource.TestCheckResourceAttr(name, "selected_projects.0.selected_branches.0", "main"),
