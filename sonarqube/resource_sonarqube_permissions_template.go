45a62ed2ea6ee842c51047ba6ec61185bb651bf4
Adam Weigert <smokedlinq@outlook.com>
2023-11-08 21:06:04 -0500

    fix set_default_template url path

--- sonarqube/resource_sonarqube_permissions_template.go
+++ sonarqube/resource_sonarqube_permissions_template.go
@@ -100,6 +100,7 @@ func resourceSonarqubePermissionTemplateCreate(d *schema.ResourceData, m interfa
 
 	// If default is set to true, set this permission template as the default.
 	if d.Get("default").(bool) {
+		sonarQubeURL = m.(*ProviderConfiguration).sonarQubeURL
 		err = resourceSonarqubePermissionTemplateSetDefault(sonarQubeURL, d.Id(), m)
 		if err != nil {
 			return err
@@ -189,6 +190,7 @@ func resourceSonarqubePermissionTemplateUpdate(d *schema.ResourceData, m interfa
 
 	// If default is set to true, set this permission template as the default.
 	if d.Get("default").(bool) {
+		sonarQubeURL = m.(*ProviderConfiguration).sonarQubeURL
 		err = resourceSonarqubePermissionTemplateSetDefault(sonarQubeURL, d.Id(), m)
 		if err != nil {
 			return err
