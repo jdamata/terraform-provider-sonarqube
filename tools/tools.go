2aab27811341578fb351ee39fbe65184da0eb70b
Goncalves Pedro (HOME/EET) <Pedro.Goncalves6@pt.bosch.com>
2025-01-03 19:39:22 +0000

    Fix tools configuration

---
+++ tools/tools.go
@@ -0,0 +1,12 @@
+//go:build tools
+
+package tools
+
+import (
+	// Documentation generation
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+)
+
+// * Run the docs generation tool
+//go:generate echo "Generate provider documentation..."
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir
