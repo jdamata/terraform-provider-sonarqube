3574c3818a7067076e5e3f34e31268498886399a
Jordan Burns <burnz2304@gmail.com>
2025-11-23 21:56:41 -0500

    Reverting the create back to the rename endpoint. Adding logic to support older SonarQube versions via deleting and renaming endpoint as well as newer versions supporting the set_main

--- sonarqube/resource_sonarqube_project_main_branch.go
+++ sonarqube/resource_sonarqube_project_main_branch.go
@@ -8,6 +8,7 @@ import (
 	"net/url"
 	"strings"
 
+	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -57,10 +58,10 @@ func resourceSonarqubeProjectMainBranch() *schema.Resource {
 
 func resourceSonarqubeProjectMainBranchCreate(d *schema.ResourceData, m interface{}) error {
 	sonarQubeURL := m.(*ProviderConfiguration).sonarQubeURL
-	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/project_branches/set_main"
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/project_branches/rename"
 
 	sonarQubeURL.RawQuery = url.Values{
-		"branch":  []string{d.Get("name").(string)},
+		"name":    []string{d.Get("name").(string)},
 		"project": []string{d.Get("project").(string)},
 	}.Encode()
 
@@ -119,17 +120,105 @@ func resourceSonarqubeProjectMainBranchRead(d *schema.ResourceData, m interface{
 	return fmt.Errorf("resourceSonarqubeProjectMainBranchRead: Failed to find project main branch: %+v", d.Id())
 }
 
-func resourceSonarqubeProjectMainBranchUpdate(d *schema.ResourceData, m interface{}) error {
-	sonarQubeURL := m.(*ProviderConfiguration).sonarQubeURL
-	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/project_branches/set_main"
-
+// getBranches retrieves all branches for a given project
+func getBranches(conf *ProviderConfiguration, projectKey string) (*GetBranches, error) {
+	sonarQubeURL := conf.sonarQubeURL
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/project_branches/list"
 	sonarQubeURL.RawQuery = url.Values{
-		"branch":  []string{d.Get("name").(string)},
-		"project": []string{d.Get("project").(string)},
+		"project": []string{projectKey},
 	}.Encode()
 
 	resp, err := httpRequestHelper(
-		m.(*ProviderConfiguration).httpClient,
+		conf.httpClient,
+		"GET",
+		sonarQubeURL.String(),
+		http.StatusOK,
+		"getBranches",
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	branchResponse := &GetBranches{}
+	err = json.NewDecoder(resp.Body).Decode(branchResponse)
+	if err != nil {
+		return nil, fmt.Errorf("getBranches: Failed to decode json into struct: %+v", err)
+	}
+
+	return branchResponse, nil
+}
+
+// deleteBranch removes a non-main branch from a project
+func deleteBranch(conf *ProviderConfiguration, projectKey, branchName string) error {
+	sonarQubeURL := conf.sonarQubeURL
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + "/api/project_branches/delete"
+	sonarQubeURL.RawQuery = url.Values{
+		"branch":  []string{branchName},
+		"project": []string{projectKey},
+	}.Encode()
+
+	resp, err := httpRequestHelper(
+		conf.httpClient,
+		"POST",
+		sonarQubeURL.String(),
+		http.StatusNoContent,
+		"deleteBranch",
+	)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func resourceSonarqubeProjectMainBranchUpdate(d *schema.ResourceData, m interface{}) error {
+	conf := m.(*ProviderConfiguration)
+	sonarQubeURL := conf.sonarQubeURL
+
+	projectKey := d.Get("project").(string)
+	targetBranchName := d.Get("name").(string)
+
+	// set_main endpoint was introduced in SonarQube 10.2
+	minimumVersionForSetMain, _ := version.NewVersion("10.2")
+	useSetMainEndpoint := conf.sonarQubeVersion.GreaterThanOrEqual(minimumVersionForSetMain)
+
+	var endpoint, paramName string
+	if useSetMainEndpoint {
+		endpoint = "/api/project_branches/set_main"
+		paramName = "branch"
+	} else {
+		// For older versions using rename, we need to check if the target branch already exists
+		// If it does and it's not the main branch, delete it first to avoid conflicts
+		branches, err := getBranches(conf, projectKey)
+		if err != nil {
+			return fmt.Errorf("resourceSonarqubeProjectMainBranchUpdate: Failed to get branches: %+v", err)
+		}
+
+		for _, branch := range branches.Branches {
+			if branch.Name == targetBranchName && !branch.IsMain {
+				// Delete the existing non-main branch before renaming
+				if err := deleteBranch(conf, projectKey, targetBranchName); err != nil {
+					return fmt.Errorf("resourceSonarqubeProjectMainBranchUpdate: Failed to delete existing branch: %+v", err)
+				}
+				break
+			}
+		}
+
+		endpoint = "/api/project_branches/rename"
+		paramName = "name"
+	}
+
+	sonarQubeURL.Path = strings.TrimSuffix(sonarQubeURL.Path, "/") + endpoint
+
+	sonarQubeURL.RawQuery = url.Values{
+		paramName: []string{targetBranchName},
+		"project": []string{projectKey},
+	}.Encode()
+
+	resp, err := httpRequestHelper(
+		conf.httpClient,
 		"POST",
 		sonarQubeURL.String(),
 		http.StatusNoContent,
@@ -140,7 +229,7 @@ func resourceSonarqubeProjectMainBranchUpdate(d *schema.ResourceData, m interfac
 	}
 	defer resp.Body.Close()
 
-	id := fmt.Sprintf("%v/%v", d.Get("project").(string), d.Get("name").(string))
+	id := fmt.Sprintf("%v/%v", projectKey, targetBranchName)
 	d.SetId(id)
 
 	return resourceSonarqubeProjectMainBranchRead(d, m)
